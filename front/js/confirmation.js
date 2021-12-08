ef5688191b1f7c8c51e83673b35b15a77d2d52b2
Adrien <helary.adrien@gmail.com>
2021-12-08 14:49:49 +0100

    Comment confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,5 +1,10 @@
 const orderIdSpan = document.getElementById('orderId');
 const orderId = new URLSearchParams(window.location.search).get('orderId');
+
+/**
+ * This function set the inner text of the orderIdSpan element with the id of the order. 
+ * @param {string} id 
+ */
 function displayOrderId(id){
     orderIdSpan.innerText = id;
 }
