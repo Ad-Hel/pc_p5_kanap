69126e8e7b49c2093afd36f94122dd6e7f2f6e32
Adrien <helary.adrien@gmail.com>
2021-12-08 14:49:16 +0100

    Comment product-page.js

--- front/js/product-page.js
+++ front/js/product-page.js
@@ -14,25 +14,38 @@ const addToCart = document.getElementById('addToCart');
  */
 let id = new URLSearchParams(window.location.search).get('id');
 
+/**
+ * This function set the attribues of the img element and set the inner text of the title and description elements.
+ * @param {object} product {id: string, name: string, price: integer, imageUrl: string, description: string, altText: string}
+ */
 function writeDetailProduct(product){
     img.setAttribute('src', product.imageUrl);
     img.setAttribute('alt', product.altText);
     title.innerText = product.name;
     description.innerText = product.description;
 }
-
+/**
+ * This function iterate the array of colors to create a html snippet with the optionTemplate function and insert it inside the colors element.
+ * The function optionTemplate is declared in the product.html at line 70.
+ * @param {array of string} productColors 
+ */
 function writeColorsOptions(productColors){
     productColors.forEach(color => {
         colors.insertAdjacentHTML('beforeend', optionTemplate(color));
     });
 }
-
+/**
+ * This function call the API fetch and pass the response to two functions.
+ */
 async function showProduct(){
     let product = await getProducts(id);
     writeDetailProduct(product);
     writeColorsOptions(product.colors);
 }
-
+/**
+ * This event lister is used ton add in the cart a new entry.
+ * This entry is built with the value of the colors select, the value of the input number quantity and the id of the product.
+ */
 addToCart.addEventListener('click', function(){
     getLocalCart();
     let color = colors.value;
